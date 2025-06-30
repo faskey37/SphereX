d5caf279b90d9f2747857a6eb6cf7efa191b5d96
khantauqeer272@gmail.com <khantauqeer272@gmail.com>
2025-06-30 20:15:40 +0530

    fb

---
+++ firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBQTKu0peEEss99dkaOxHK3lw_nmhgAX_Q",
+  authDomain: "fir-ai-app-d19e6.firebaseapp.com",
+  projectId: "fir-ai-app-d19e6",
+  storageBucket: "fir-ai-app-d19e6.firebasestorage.app",
+  messagingSenderId: "655293858757",
+  appId: "1:655293858757:web:8cc5c4d98c7ba96fb381ac"
+};
+firebase.initializeApp(firebaseConfig);
